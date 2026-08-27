6809fb462ee2a2f0eee29097b9f3b68a3c9b3c08
LinPyaeBo <144644416+LinPyaeBo@users.noreply.github.com>
2026-08-27 13:35:32 +0630

    first commit

---
+++ CloudDriveBackend/src/main/java/com/linpyaebo/clouddrivebackend/CloudDriveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.linpyaebo.clouddrivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDriveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDriveBackendApplication.class, args);
+    }
+
+}
---
+++ CloudDriveBackend/src/test/java/com/linpyaebo/clouddrivebackend/CloudDriveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linpyaebo.clouddrivebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDriveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
